985dec99feaa365ec1ffcdb34e7f5603316c8fb2
Jackie <jye0325@gmail.com>
2022-07-23 04:29:30 -0400

    commit tree 5897c021c8d27592fa909edc870c52f6dc1fdb0c

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index {|elem, index| puts "#{index+1}. #{elem}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map(&:capitalize).map {|string| string << "!"} 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.find {|word| word.length > 4} ? true : false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|element| cheese_types.include?(element) }
 end
